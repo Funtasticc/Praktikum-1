56985f4557f978d2daccb6209c320ecbec8206e0
Steffen <steffen.moeller1993@gmx.net>
2016-10-10 02:23:41 +0200

    Add files via upload

---
+++ hallo_welt.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*Programm zur Ausgabe
+	von "Hallo Welt!" 
+	auf dem Bildschirm*/
+
+	cout << "Hallo Welt!\n"; 
+	
+	/*Der Testlauf wird
+	hier stoppen: Taste drücken*/
+
+	system("pause"); 
+
+	/*Rückgabewert 0: fehler-
+	freie Programmausführung!*/
+
+	return 0;
+}
